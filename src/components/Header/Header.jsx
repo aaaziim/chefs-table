33c2e24c76b6aee8b50fddace5ba53926c170d62
aaaziim <azim210215@gmail.com>
2024-12-05 15:57:24 +0600

    Header ICons Fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import Banner from "../../assets/Images/bgbanner.jpg"
+import { CiSearch } from "react-icons/ci";
+import { FaUser } from "react-icons/fa";
 const Header = () => {
     return (
         <>
@@ -15,15 +17,21 @@ const Header = () => {
                     <li>Search</li>
                 </ul>
                 <div>
-                    <p>
-                        <input className="rounded-2xl px-4 py-2 bg-gray-200" type="text" placeholder="Search" id=""></input>
-                    </p>
-                    {/* <p>
-        <img src="" alt=""> </img>
-    </p> */}
+                    <div className="relative flex items-center">
+                        <CiSearch className="absolute left-3 text-gray-500" />
+                        <input
+                            className="rounded-2xl px-10 py-2 bg-gray-200 w-full"
+                            type="text"
+                            placeholder="Search"
+                            id=""
+                        />
+                        <p className="px-3 text-2xl"> <FaUser /></p>
+
+                    </div>
+
                 </div>
 
-            </div>
+            </div >
             <section
                 style={{ backgroundImage: `url(${Banner})` }}
                 className="bg-cover bg-center h-screen text-center flex justify-center items-center"
